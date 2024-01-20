e980db2c7946c9b69ac9d47746e288dee5eb197d
Anastasiia <na.konsta@gmail.com>
2024-01-20 21:09:10 +0100

    init

---
+++ index.js
@@ -0,0 +1,17 @@
+// REVERT PACKAGE.JSON TYPE TO COMMON.JS
+
+require('dotenv').config();
+const { MongoClient, ObjectId } = require('mongodb');
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+app.use('/api/users', require('./users'));
+app.use((err, req, res, next) => res.status(500).send(err.message));
+
+const port = process.env.PORT || 3004;
+
+app.listen(port, () => {
+  console.log(`  Listening on http://localhost:${port}`);
+});
