4e314548548a5e67ef6956d4d1d2400cc47309fe
Anastasiia <na.konsta@gmail.com>
2024-01-21 01:34:07 +0100

    with server

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 // import {MongoClient, ObjectId} from 'mongodb'
 import router from './users.js';
+import http from 'http';
 dotenv.config();
 // const express = require('express');
 
@@ -11,10 +12,11 @@ const app = express();
 app.use(express.json());
 app.use('/api/users', router);
 app.use((err, req, res, next) => res.status(500).send(err.message));
+const server = http.createServer(app);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`  Listening on http://localhost:${port}`);
 });
 
